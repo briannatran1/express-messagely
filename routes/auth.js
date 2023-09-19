2b571dd00a5afed086aeee1e0581849e6fc59866
Brianna Tran <briannatran45@gmail.com>
2023-09-18 17:20:00 -0700

    started routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,36 @@
 "use strict";
 
+const { SECRET_KEY } = require("../config");
+const { BadRequestError, UnauthorizedError } = require("../expressError");
+const User = require("../models/user");
+const jwt = require('jsonwebtoken');
+
 const Router = require("express").Router;
 const router = new Router();
 
 /** POST /login: {username, password} => {token} */
+router.post('/login', async function (req, res) {
+  if (req.body === undefined) throw new BadRequestError();
+
+  const { username, password } = req.body;
+  if (await User.authenticate(username, password)) {
+    let payload = { username };
+    let token = jwt.sign(payload, SECRET_KEY);
+
+    return res.json({ token });
+  }
+
+  throw new UnauthorizedError('Invalid credentials');
+});
 
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+
+router.post('/register', async function (req, res) {
+
+});
 
 module.exports = router;
